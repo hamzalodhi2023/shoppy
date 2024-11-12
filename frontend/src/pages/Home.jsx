f77b0f061a435f6716ac514365c13ab3d84da43f
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-11-12 14:15:30 +0500

    "Added Slogan component to Home page"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import NewArrivals from "../components/NewArrivals"
 import Services from "../components/Services"
 import Slider from "../components/Slider"
+import Slogan from "../components/Slogan"
 function Home() {
     return (
         <>
             <Slider />
             <Services />
             <NewArrivals />
+            <Slogan />
         </>
     )
 }
