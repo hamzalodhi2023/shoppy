916a3fc874ba2129d02bb686e508a099430d22b8
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-11-16 22:15:30 +0500

    "Updated Slogan component styling and added rating schema to product model also modified product model"

--- server/models/product.js
+++ server/models/product.js
@@ -1,6 +1,115 @@
 const { Schema, model } = require("mongoose");
 
-const productSchema = new Schema({});
+const ratingSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    colors: {
+      type: Array,
+      default: null,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    subCategory: {
+      type: String,
+      required: true,
+    },
+    unitOfMeasurement: {
+      type: String,
+      required: true,
+    },
+    unit: {
+      type: String,
+      default: null,
+    },
+    measurement: {
+      type: String,
+      default: null,
+    },
+    material: {
+      type: String,
+      default: null,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    stuffType: {
+      type: String,
+      default: null,
+    },
+    madeType: {
+      type: String,
+      enum: ["Stitched", "Non Stitched"],
+      default: "null",
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    stockStatus: {
+      type: String,
+      enum: ["In Stock", "Out of Stock"],
+      required: true,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    sale: {
+      type: Boolean,
+      default: false,
+    },
+    rating: [
+      {
+        type: ratingSchema,
+        default: null,
+      },
+    ],
+    photos: [
+      {
+        type: Buffer,
+        required: true,
+        min: 3,
+        max: 5,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const productModel = model("product", productSchema);
 module.exports = productModel;
