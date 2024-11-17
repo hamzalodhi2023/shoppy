d5c62951aac9e992028621ff04435ee1424ee9c3
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-11-17 13:08:01 +0500

    "Updated .gitignore to include .env.example and modified create.js to update dropdown values"

--- server/controllers/dropdowns/create.js
+++ server/controllers/dropdowns/create.js
@@ -4,7 +4,9 @@ const debug = require("debug")("development:controllers:dropdowns:create.js");
 module.exports = async (req, res) => {
   try {
     const { name, values } = req.body;
+
     const dropdown = await dropdownModel.findOne({ name });
+
     if (!dropdown) {
       debug("dropdown not found");
       return res.status(404).json({
@@ -12,9 +14,13 @@ module.exports = async (req, res) => {
         message: "Dropdown not found.",
       });
     }
-    const newValues = [...dropdown.values, ...values];
-          
 
+    const newValues = [...dropdown.values, ...values];
+    const updatedDropdown = await dropdownModel.findByIdAndUpdate(
+      dropdown._id,
+      { values: newValues },
+      { new: true }
+    );
   } catch (error) {
     res.json(error);
   }
