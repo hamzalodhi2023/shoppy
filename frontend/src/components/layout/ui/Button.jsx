d3ee56abcb9db36e031f4be5215d675752445dbd
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-11-21 15:02:01 +0500

    "Updated Button components to use conditional classnames for width based on 'size' prop"

--- frontend/src/components/layout/ui/Button.jsx
+++ frontend/src/components/layout/ui/Button.jsx
@@ -1,7 +1,7 @@
 export const Button1 = ({ text, onclick, size, type }) => {
     return (
         <button
-            className={`btn w-${size} bg-gradient-to-b from-[#FF7E31] to-[#E62C03] text-white rounded-md shadow-md border-none hover:shadow-[rgba(253,76,0,0.5)_0_3px_8px] h-10 px-5`}
+            className={`btn ${size === "fit" ? "w-fit" : "w-full"} bg-gradient-to-b from-[#FF7E31] to-[#E62C03] text-white rounded-md shadow-md border-none hover:shadow-[rgba(253,76,0,0.5)_0_3px_8px] h-10 px-5`}
             role="button"
             onClick={onclick}
             type={type}
@@ -14,7 +14,7 @@ export const Button2 = ({ text, onclick, size }) => {
     return (
         <button
             onClick={onclick}
-            className={`btn w-${size} bg-gradient-to-b from-[#747474] to-[#383838] text-white rounded-md shadow-md border-none hover:shadow-[rgba(56,56,56,0.5)_0_3px_8px] h-10 px-5`}>
+            className={`btn ${size === "fit" ? "w-fit" : "w-full"} bg-gradient-to-b from-[#747474] to-[#383838] text-white rounded-md shadow-md border-none hover:shadow-[rgba(56,56,56,0.5)_0_3px_8px] h-10 px-5`}>
             {text}
         </button>
     )
