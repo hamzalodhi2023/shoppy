12d43948b755ea87a62cd7d6322d9f8ecb54cc4f
Hamza Khna Lodhi <hamzalodhi2023@gmail.com>
2024-12-25 14:08:14 +0500

    `Refactor ProductStore.jsx and Home.jsx to use React Query and Redux`

--- frontend/src/api/ProductStore.jsx
+++ frontend/src/api/ProductStore.jsx
@@ -1,13 +1,22 @@
-const initialState = { product: [] }
+import { useQuery } from "@tanstack/react-query"
+import { GetAllProducts } from "../api/ProductApi"
+import { createStore } from 'redux'
+import { useProducts } from "./ProductMutations"
+
+const { data } = useProducts()
+console.log(data)
+const initialState = { products: data }
 
 const productReducer = (state = initialState, action) => {
     switch (action.type) {
-        case value:
-
-            break;
-
-        default:
-            break;
+        case "SET_PRODUCTS":
+            return { ...state, products: action.payload };
+        default: return state
     }
 
 }
+
+// Store
+
+const store = createStore(productReducer)
+console.log(store)
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import { useQuery } from "@tanstack/react-query"
-import { GetAllProducts } from "../api/ProductApi"
 import FeaturedProducts from "../components/FeaturedProducts"
 import LatestBlog from "../components/LatestBlog"
 import NewArrivals from "../components/NewArrivals"
@@ -7,10 +5,7 @@ import Services from "../components/Services"
 import Slider from "../components/Slider"
 import Slogan from "../components/Slogan"
 import Subscribe from "../components/Subscribe"
-import { useProducts } from "../api/ProductMutations"
 function Home() {
-    const { data } = useProducts()
-    console.log(data)
 
     return (
         <>
