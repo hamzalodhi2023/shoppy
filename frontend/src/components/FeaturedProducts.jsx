b60324b05c299b0d0472025b4e4a6fb8c77bb410
Hamza Khna Lodhi <hamzalodhi2023@gmail.com>
2024-12-26 23:44:55 +0500

    `feat: Added useSelector hook and mapped products to Cards component in FeaturedProducts and NewArrivals components`

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -1,14 +1,19 @@
+import { useSelector } from "react-redux";
 import Cards from "./layout/ui/Cards"
 import Title from "./layout/ui/Title"
 
 function FeaturedProducts() {
+    const data = useSelector((state) => state.products);
+    const products = data.slice(0, 3);
     return (
         <div className="w-full py-16 px-4 dark:bg-[#383838]">
             <div className="max-w-[1240px] mx-auto">
                 <Title text1={"FEATURED"} text2={"PRODUCTS"} />
-                <div className="flex flex-wrap gap-6 justify-evenly">
+                <div className="flex flex-wrap justify-evenly">
                     {/* cards Start */}
-
+                    {products.map(({ _id, name, price, description, photos }) => {
+                        return <Cards key={_id} _id={_id} name={name} price={price} description={description} photos={photos} />
+                    })}
                     {/* cards End */}
                 </div>
             </div>
--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from "react-redux";
 import Title from "./layout/ui/Title"
+import Cards from "./layout/ui/Cards";
 
 function NewArrivals() {
-    const products = useSelector((state) => state.products);
-    console.log(products)
+    const data = useSelector((state) => state.products);
+    const products = data.slice(0, 3);
     return (
         <div className="w-full py-16 px-4 dark:bg-[#383838]">
             <div className="max-w-[1240px] mx-auto">
                 <Title text1={"NEW"} text2={"ARRIVALS"} />
-                <div className="flex flex-wrap gap-6 justify-evenly">
+                <div className="flex flex-wrap justify-evenly">
                     {/* cards Start */}
-
+                    {products.map(({ _id, name, price, description, photos }) => {
+                        return <Cards key={_id} _id={_id} name={name} price={price} description={description} photos={photos} />
+                    })}
                     {/* cards End */}
                 </div>
             </div>
