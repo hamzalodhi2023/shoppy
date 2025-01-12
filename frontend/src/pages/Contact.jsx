155a6db32fd57a09c96b513d08074cecfddce475
Hamza Khna Lodhi <hamzalodhi2023@gmail.com>
2025-01-12 14:44:56 +0500

    `Fixed typo in Contact page paragraph and updated input field text colors`

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -5,7 +5,7 @@ import Title from "../components/layout/ui/Title"
 function Contact() {
     return (
         <div className="min-h-screen bg-white flex-col dark:bg-[#383838] flex items-center justify-center">
-            <Hero img={"/images/contact-us.png"} heading="Get in Touch with Us!" paragraph="At Shoppy, your satisfaction is our top priority. Whether you have a question about your order, need help with a product, or just want to share feedback, we’re here for you." />
+            <Hero img={"/images/contact-us.png"} heading="Get in Touch with Us!" paragraph="At Shoppy, your satisfaction is our top priority. Whether you have a question about your order, need help with a product, or just want to share feedback, we're here for you." />
             <div className="max-w-3xl my-5 w-full bg-gray-100 dark:bg-[#2a2a2a] rounded-lg shadow-lg p-6 md:p-8">
                 <Title text1={"CONTACT"} text2={"US"} />
                 <form className="space-y-6">
@@ -14,14 +14,14 @@ function Contact() {
                             <label className="block mb-2 text-gray-800 dark:text-gray-200">First Name</label>
                             <input
                                 type="text"
-                                className="w-full bg-white px-4 py-2 rounded-lg border dark:border-gray-600 focus:outline-none focus:border-[#e97730] dark:focus:border-[#e97730] dark:bg-[#383838] dark:text-gray-200"
+                                className="w-full bg-white px-4 py-2 rounded-lg border dark:border-gray-600 focus:outline-none focus:border-[#e97730] dark:focus:border-[#e97730] dark:bg-[#383838] text-[#383838] dark:text-gray-300"
                             />
                         </div>
                         <div>
                             <label className="block mb-2 text-gray-800 dark:text-gray-200">Last Name</label>
                             <input
                                 type="text"
-                                className="w-full dark:focus:border-[#e97730] px-4 py-2 bg-white rounded-lg border dark:border-gray-600 focus:outline-none focus:border-[#e97730] dark:bg-[#383838] dark:text-gray-200"
+                                className="w-full dark:focus:border-[#e97730] px-4 py-2 bg-white rounded-lg border dark:border-gray-600 focus:outline-none focus:border-[#e97730] dark:bg-[#383838] text-[#383838] dark:text-gray-300"
                             />
                         </div>
                     </div>
@@ -29,14 +29,14 @@ function Contact() {
                         <label className="block mb-2 text-gray-800 dark:text-gray-200">Email</label>
                         <input
                             type="email"
-                            className="w-full px-4 py-2 bg-white rounded-lg border dark:border-gray-600 focus:outline-none focus:border-[#e97730] dark:focus:border-[#e97730] dark:bg-[#383838] dark:text-gray-200"
+                            className="w-full px-4 py-2 bg-white rounded-lg border dark:border-gray-600 focus:outline-none focus:border-[#e97730] dark:focus:border-[#e97730] dark:bg-[#383838] text-[#383838] dark:text-gray-300"
                         />
                     </div>
                     <div>
                         <label className="block mb-2 text-gray-800 dark:text-gray-200">Message</label>
                         <textarea
                             rows="4"
-                            className="w-full px-4 py-2 rounded-lg bg-white border dark:border-gray-600 focus:outline-none dark:focus:border-[#e97730] focus:border-[#e97730] dark:bg-[#383838] dark:text-gray-200"
+                            className="w-full px-4 py-2 rounded-lg bg-white border dark:border-gray-600 focus:outline-none dark:focus:border-[#e97730] focus:border-[#e97730] dark:bg-[#383838] text-[#383838] dark:text-gray-300"
                         ></textarea>
                     </div>
                     <Button1 text={"Send Message"} size={"full"} />
